f8a58eddd8712f66ed3a857c196da7e59c62277a
Oleksii Tkachuk <tkalexey@gmail.com>
2022-05-23 11:56:12 +0300

    Add class Car

---
+++ src/main/java/org/fwork/objects/Car.java
@@ -0,0 +1,12 @@
+package org.fwork.objects;
+
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
+public class Car {
+    private String maker;
+    private String model;
+    private String modification;
+}
